f49bdaf061d7239b5bde4c539f7904d59bb24c94
阿星 <jiangzengxing@xiaomi.com>
2020-06-28 21:51:49 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         'class-methods-use-this': 'off',
         'no-unused-vars': 'off',
         'no-console': 'off',
+        'no-alert': 'off',
         indent: ['error', 4],
         'max-len': [
             'error',
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import anime from 'animejs';
 import { getSearch } from '../common/utils.ts';
+import './app.less';
 
+function timeStampFormat(timeStamp) {
+}
 export default class App extends Component {
     state = {
         menuList: [
@@ -12,6 +14,10 @@ export default class App extends Component {
             { name: '2', show: true },
             { name: '2', show: true },
         ],
+        messageList: [
+            { message: 'asdad', time: 1593348843396, sender: 'usr' },
+            { message: '憨八龟', time: 1593348843899, sender: 'robot' },
+        ],
     }
 
     get isAdmin() {
@@ -19,156 +25,79 @@ export default class App extends Component {
     }
 
     componentDidMount() {
-        // this.bgInit();
-    }
-
-    bgInit() {
-        const canvasEl = this.refs['main-bg'];
-        console.log(canvasEl);
-        const ctx = canvasEl.getContext('2d');
-        const numberOfParticules = 30;
-        let pointerX = 0;
-        let pointerY = 0;
-        const tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
-        const colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
-
-        function setCanvasSize() {
-            canvasEl.width = window.innerWidth * 2;
-            canvasEl.height = window.innerHeight * 2;
-            canvasEl.style.width = `${window.innerWidth}px`;
-            canvasEl.style.height = `${window.innerHeight}px`;
-            canvasEl.getContext('2d').scale(2, 2);
-        }
-
-        function updateCoords(e) {
-            pointerX = e.clientX || e.touches[0].clientX;
-            pointerY = e.clientY || e.touches[0].clientY;
-        }
-
-        function setParticuleDirection(p) {
-            const angle = (anime.random(0, 360) * Math.PI) / 180;
-            const value = anime.random(50, 180);
-            const radius = [-1, 1][anime.random(0, 1)] * value;
-            return {
-                x: p.x + radius * Math.cos(angle),
-                y: p.y + radius * Math.sin(angle),
-            };
-        }
-
-        function createParticule(x, y) {
-            const p = {};
-            p.x = x;
-            p.y = y;
-            p.color = colors[anime.random(0, colors.length - 1)];
-            p.radius = anime.random(16, 32);
-            p.endPos = setParticuleDirection(p);
-            p.draw = () => {
-                ctx.beginPath();
-                ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-                ctx.fillStyle = p.color;
-                ctx.fill();
-            };
-            return p;
-        }
-
-        function createCircle(x, y) {
-            const p = {};
-            p.x = x;
-            p.y = y;
-            p.color = '#FFF';
-            p.radius = 0.1;
-            p.alpha = 0.5;
-            p.lineWidth = 6;
-            p.draw = () => {
-                ctx.globalAlpha = p.alpha;
-                ctx.beginPath();
-                ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-                ctx.lineWidth = p.lineWidth;
-                ctx.strokeStyle = p.color;
-                ctx.stroke();
-                ctx.globalAlpha = 1;
-            };
-            return p;
-        }
-
-        function renderParticule(anim) {
-            for (let i = 0; i < anim.animatables.length; i += 1) {
-                anim.animatables[i].target.draw();
-            }
-        }
-
-        function animateParticules(x, y) {
-            const circle = createCircle(x, y);
-            const particules = [];
-            for (let i = 0; i < numberOfParticules; i += 1) {
-                particules.push(createParticule(x, y));
-            }
-            anime.timeline().add({
-                targets: particules,
-                x(p) { return p.endPos.x; },
-                y(p) { return p.endPos.y; },
-                radius: 0.1,
-                duration: anime.random(1200, 1800),
-                easing: 'easeOutExpo',
-                update: renderParticule,
-            })
-                .add({
-                    targets: circle,
-                    radius: anime.random(80, 160),
-                    lineWidth: 0,
-                    alpha: {
-                        value: 0,
-                        easing: 'linear',
-                        duration: anime.random(600, 800),
-                    },
-                    duration: anime.random(1200, 1800),
-                    easing: 'easeOutExpo',
-                    update: renderParticule,
-                    offset: 0,
-                });
-        }
-
-        const render = anime({
-            duration: Infinity,
-            update() {
-                ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-            },
-        });
-
-        document.addEventListener(tap, (e) => {
-            render.play();
-            updateCoords(e);
-            animateParticules(pointerX, pointerY);
-        }, false);
-
-        function autoClick() {
-            animateParticules(
-                anime.random(100, window.innerWidth - 100),
-                anime.random(100, window.innerHeight - 100),
-            );
-            anime({ duration: 200 }).finished.then(autoClick);
-        }
-
-        autoClick();
-        setCanvasSize();
-        window.addEventListener('resize', setCanvasSize, false);
+        window.app = this;
     }
 
     openUrl(url) {
+        if (!url) return;
         const { location } = window;
         location.pathname = url;
     }
 
+    autoResponse(message) {
+        const { menuList = '' } = this.state;
+        const res = menuList.find((item) => message.indexOf(item.name) !== -1);
+        if (res && res.show) {
+            return <a href={res.url}>{res.name}</a>;
+        }
+        return '你个憨八🐢，给👴爬';
+    }
+
+    sendMessage(e) {
+        const { input: inputDiv } = this.refs;
+        window.asd = this.refs;
+        if (inputDiv && inputDiv.innerText && e.which === 13) {
+            const message = inputDiv.innerText;
+            this.setMessage({ message, time: +new Date(), sender: 'usr' }, () => {
+                setTimeout(() => {
+                    this.setMessage({ message: this.autoResponse(message), time: +new Date(), sender: 'robot' });
+                }, 500);
+            });
+            setTimeout(() => {
+                inputDiv.innerHTML = '';
+            }, 0);
+        }
+    }
+
+    setMessage(message, callback = () => {}) {
+        const messageList = [...this.state.messageList];
+        messageList.push(message);
+        this.setState({
+            messageList,
+        }, callback);
+    }
+
     render() {
-        const { menuList } = this.state;
+        const { menuList, messageList } = this.state;
         return <div className="background">
-            {/* <canvas style={{
-                position: 'absolute', top: 0, bottom: 0, left: 0, right: 0,
-            }} ref="main-bg">
-            </canvas> */}
-            <div className="box" >
-                {menuList.filter((item) => item.show)
-                    .map((item, index) => <div onClick={() => { this.openUrl(item.url); }} key={index}>{ item.name }</div>)}
+            <div className="chat-room">
+                <div className="usr-info">
+                    <img className='left-info' src="./src/hanbag.png" onClick={() => { window.open('./src/hanbag.png'); }}></img>
+                    <div className="right-info">
+                        <div className="name">精神小伙杰尼🐢</div>
+                        <div className="sign">大🍔批，真🦷么真🍔批</div>
+                    </div>
+                </div>
+                <div className="chat-info">
+                    <div className="send-info">
+                        {this.isAdmin && <div className="admin-power">
+                            {menuList.map((item) => <a style={{ padding: '0 10px' }} href={item.url}>{item.name}</a>)}
+                        </div> }
+                        {messageList.map((item, index) => {
+                            const { sender = 'usr' } = item;
+                            return (<div key={index} className={[sender, 'message'].join(' ')}>
+                                <img
+                                    src={sender === 'robot' ? './src/hanbag.png' : './src/daidais.png'}
+                                    onClick={() => { window.open(`${sender === 'robot' ? './src/hanbag.png' : './src/daidais.png'}`); }}/>
+                                <div style={{ display: 'flex' }}>
+                                    <span className="bubble-box">{item.message}</span>
+                                </div>
+                            </div>);
+                        })}
+                    </div>
+                    <div className="drive-line"></div>
+                    <div contentEditable={true} ref="input" onKeyDown={this.sendMessage.bind(this)} className="edit-info"></div>
+                </div>
             </div>
         </div>;
     }
